1f9cc3d3e54557b56823cc27d20371ea84505fcd
Rocky <rrsalerno21@gmail.com>
2020-08-11 11:14:14 -0700

    Updates

---
+++ precache-manifest.83be59547b3503a407bb75fcaa21804a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c71aba9079cabd955a6af34d1a264e",
+    "url": "/rrsalerno21.github.io/index.html"
+  },
+  {
+    "revision": "e489df984d2772562367",
+    "url": "/rrsalerno21.github.io/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "6532e7774da45611f49d",
+    "url": "/rrsalerno21.github.io/static/css/main.a5410a99.chunk.css"
+  },
+  {
+    "revision": "e489df984d2772562367",
+    "url": "/rrsalerno21.github.io/static/js/2.598d1519.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rrsalerno21.github.io/static/js/2.598d1519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6532e7774da45611f49d",
+    "url": "/rrsalerno21.github.io/static/js/main.e305ebf4.chunk.js"
+  },
+  {
+    "revision": "87dca5704daec7a8561c",
+    "url": "/rrsalerno21.github.io/static/js/runtime-main.849bb326.js"
+  },
+  {
+    "revision": "fbb7671a228eccdf1cf16d916d5aee5f",
+    "url": "/rrsalerno21.github.io/static/media/forget-me-not-app-dashboard-jpg.fbb7671a.jpg"
+  },
+  {
+    "revision": "8f961800622ae148c041f16b4651bd1f",
+    "url": "/rrsalerno21.github.io/static/media/note-taker-app.8f961800.jpg"
+  },
+  {
+    "revision": "ebfeb43e082d08292a31d85cf022d116",
+    "url": "/rrsalerno21.github.io/static/media/robert-salerno-full-stack-developer-resume.ebfeb43e.pdf"
+  },
+  {
+    "revision": "30547c6687bf707435f74afe1d58c373",
+    "url": "/rrsalerno21.github.io/static/media/rocky-photo.30547c66.jpg"
+  },
+  {
+    "revision": "acb564a9a53778b3f14380ac06f6647d",
+    "url": "/rrsalerno21.github.io/static/media/seefood-screenshot.acb564a9.png"
+  },
+  {
+    "revision": "5e386b5706d3e9b826922cfe30fccb63",
+    "url": "/rrsalerno21.github.io/static/media/to-do-list-app.5e386b57.jpg"
+  },
+  {
+    "revision": "892eb07bd3e5aaa0ac372d5db98ffae0",
+    "url": "/rrsalerno21.github.io/static/media/weather-app-screenshot.892eb07b.png"
+  },
+  {
+    "revision": "7acd88c568e88630a2aa15f204a234c2",
+    "url": "/rrsalerno21.github.io/static/media/workday-calendar-app-screenshot.7acd88c5.png"
+  }
+]);
